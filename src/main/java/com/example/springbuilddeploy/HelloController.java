98b4d7f20c51b26d0fea300cc6f622e70080ab6f
agaray <angaray93@gmail.com>
2022-07-13 09:12:46 -0400

    spring heroku app config

---
+++ src/main/java/com/example/springbuilddeploy/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.springbuilddeploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")
+    String message;
+
+    @GetMapping("/hola")
+    public String holaMundo() {
+        System.out.println(message);
+        return "Hola mundo";
+    }
+}
---
+++ src/test/java/com/example/springbuilddeploy/SpringBuildDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbuilddeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBuildDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
